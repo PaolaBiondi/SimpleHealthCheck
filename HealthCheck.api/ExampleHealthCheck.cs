2446ef933030fca0b88c23696306b16564847c4c
Paola <ml3oepl0@duck.com>
2025-11-02 23:38:41 +0100

    Add project files.

---
+++ HealthCheck.api/ExampleHealthCheck.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+internal class ExampleHealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        if (DateTime.UtcNow.Second % 2 == 0)
+        {
+            return Task.FromResult(HealthCheckResult.Unhealthy("The check indicates an unhealthy result."));
+        }
+
+        return Task.FromResult(HealthCheckResult.Healthy("The check indicates a healthy result."));
+    }
+}
